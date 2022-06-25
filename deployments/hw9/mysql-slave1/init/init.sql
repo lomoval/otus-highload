7012eb56f670d3291a5dc6db2a9bbdc7848c0957
Александр Ломов <a.lomov@sysdyn.ru>
2022-06-25 18:17:44 +0300

    Add 09 homework: add docker-compose configs for mysql, nginx, haproxy

---
+++ deployments/hw9/mysql-slave1/init/init.sql
@@ -0,0 +1,12 @@
+CREATE USER 'user'@'%' IDENTIFIED BY 'pass';
+GRANT ALL PRIVILEGES ON *.* TO 'user'@'%' WITH GRANT OPTION;
+ALTER USER 'user'@'%' IDENTIFIED WITH mysql_native_password BY 'pass';
+
+#CREATE USER 'root'@'%' IDENTIFIED BY 'pass';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%'  WITH GRANT OPTION;
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'pass';
+FLUSH PRIVILEGES;
+
+CREATE DATABASE db;
+
+CHANGE MASTER TO MASTER_HOST='mysql-main', MASTER_USER='root', MASTER_PASSWORD='pass',MASTER_LOG_FILE = 'binlog.000002', MASTER_LOG_POS = 336;
---
+++ deployments/hw9/mysql-slave2/init/init.sql
@@ -0,0 +1,10 @@
+CREATE USER 'user'@'%' IDENTIFIED BY 'pass';
+GRANT ALL PRIVILEGES ON *.* TO 'user'@'%' WITH GRANT OPTION;
+ALTER USER 'user'@'%' IDENTIFIED WITH mysql_native_password BY 'pass';
+
+CREATE USER 'root'@'%' IDENTIFIED BY 'pass';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%'  WITH GRANT OPTION;
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'pass';
+FLUSH PRIVILEGES;
+
+CREATE DATABASE db;
